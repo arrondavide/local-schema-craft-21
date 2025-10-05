64857b0307f3f5f6d19de42582dc65b444523e5b
Aaron <arrondavide@gmail.com>
2025-10-05 20:08:33 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import BusinessSchema from "./pages/BusinessSchema"; // Your existing BusinessForm
 import ServiceSchema from "./pages/ServiceSchema";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
@@ -19,8 +20,8 @@ const App = () => (
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/business" element={<BusinessSchema />} />
           <Route path="/service" element={<ServiceSchema />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
